7e0b7615faa42c937eea37b33bad84e1d0f2cfc8
Ross Markello <rossmarkello@gmail.com>
2018-08-10 13:48:34 -0700

    [ENH] Better notebooks
    Getting things set up for the 2018-08-10 NHA2018 presentations

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 A small project for making plots of hackathon statistics. Designed at Neurohackademy 2018.
 
 # Installation and setup
-It is _strongly_ encouraged you install via the following in a new conda environment or your code will very likely **not work**. Sorry!
+It is _strongly_ encouraged you install via the following in a new `conda` environment or your code will very likely **not work**. Sorry!
 
 ```bash
 # create a new conda env with the required modules
@@ -16,19 +16,18 @@ cd hacktrack
 pip install -e .
 ```
 
-You will also want to get a Github personal access token by signing in to Github and navigating to [https://github.com/settings/tokens](https://github.com/settings/tokens). It would be wise to select read-only for the majority of the options and save the access token generated. Set it up as an environmental variable `GITHUB_API` in the form username:key, as in:
+You will also want to get a GitHub personal access token by signing in to GitHub and navigating to [https://github.com/settings/tokens](https://github.com/settings/tokens). It would be wise to select read-only for the majority of the options and save the access token generated. Set it up as an environmental variable `GITHUB_API` in the form username:key, as in:
 
 ```bash
 $ export GITHUB_API=rmarkello:1239012803218032981390218930218939021323
 ```
 
 # Usage
-
 ```python
 >>> from hacktrack import PROJECT_LIST, get_project_info
 >>> commits, issues = get_project_info(PROJECT_LIST, update=True)
 ```
 
-`commits` and `issues` are both pandas dataframes with a whole bunch of information about commits and issues.
+`commits` and `issues` are both pandas `DataFrame` objects with a whole bunch of information about commits and issues.
 
-You can also check out `hacktrack.plotting` to do some fun plotting things.
+You can also check out `hacktrack.plotting` to do some fun plotting things!
--- hacktrack/plotting.py
+++ hacktrack/plotting.py
@@ -3,21 +3,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
-from hacktrack.scrape import (get_project_info, PROJECT_LIST)
 sns.set(style='white')
 
 
-def plot_commits_by_project(since='2018-08-07', datadir=None, **kwargs):
+def plot_commits_by_project(commits, ax=None):
     """
     Makes bar plot of commits by project
 
     Parameters
     ----------
-    since : str, optional
-        Date (YYYY-MM-DD) from which to pull information on projects:
-        Default: 2018-08-07
-    datadir : str, optional
-        Path to where data on projects should be stored. Default: None
+    commits : pd.core.data.DataFrame
+        Information on commits from projects
 
     Returns
     -------
@@ -25,32 +21,29 @@ def plot_commits_by_project(since='2018-08-07', datadir=None, **kwargs):
         Axis with plot
     """
     # get project information
-    info = get_project_info(PROJECT_LIST, datadir=datadir,
-                            since=since, verbose=False)[0]
-    data = info.groupby('project').count().sha
+    data = commits.groupby('project').count().sha
 
     # make plot!
-    ax = sns.barplot(data.values, data.index, **kwargs)
+    ax = sns.barplot(data.values, data.index, ax=ax)
     sns.despine(ax=ax)
-    ax.set(xlabel='Number of commits', ylabel='')
+    ax.set_title('Total commits over time')
+    ax.set_xlabel('Number of commits', labelpad=20)
+    ax.set_ylabel('Project', labelpad=20)
     ax.figure.tight_layout()
 
     return ax
 
 
-def plot_commits_by_user(project=None, since='2018-08-07', datadir=None):
+def plot_commits_by_user(commits, project=None, ax=None):
     """
     Makes bar plot of commits by user
 
     Parameters
     ----------
+    commits : pd.core.data.DataFrame
+        Information on commits from projects
     project : str, optional
         Name of project to plot commits from. Default: None
-    since : str, optional
-        Date (YYYY-MM-DD) from which to pull information on projects:
-        Default: 2018-08-07
-    datadir : str, optional
-        Path to where data on projects should be stored. Default: None
 
     Returns
     -------
@@ -58,33 +51,34 @@ def plot_commits_by_user(project=None, since='2018-08-07', datadir=None):
         Axis with plot
     """
     # get project information
-    info = get_project_info(PROJECT_LIST, datadir=datadir,
-                            since=since, verbose=False)[0]
     if project is not None:
         if not isinstance(project, list):
             project = [project]
-        info = info[info.project.isin(project)]
+        commits = commits[commits.project.isin(project)]
 
     # grab number of commits by user
-    info = info.query('user!=""')
-    user, counts = np.unique(info.user, return_counts=True)
+    commits = commits.query('user!=""')
+    user, counts = np.unique(commits.user, return_counts=True)
 
     # make plot!
-    ax = sns.barplot(counts, user)
+    ax = sns.barplot(counts, user, ax=ax)
     sns.despine(ax=ax)
-    ax.set(xlabel='Number of commits')
+    ax.set_xlabel('Number of commits', labelpad=20)
+    ax.set_ylabel('Users', labelpad=20)
     ax.figure.tight_layout()
 
     return ax
 
 
-def plot_commits_by_time(project=None, since='2018-08-07', frequency='10H',
-                         datadir=None):
+def plot_commits_by_time(commits, project=None, since='2018-08-07',
+                         frequency='10H', ax=None):
     """
     Plots commits in `project` over time
 
     Parameters
     ----------
+    commits : pd.core.data.DataFrame
+        Information on commits from projects
     project : str, optional
         Name of project to plot commits from
     since : str, optional
@@ -92,8 +86,6 @@ def plot_commits_by_time(project=None, since='2018-08-07', frequency='10H',
         Default: 2018-08-07
     frequency : str, optional
         Time chunks with which to plot commits over time. Default: '10H'
-    datadir : str, optional
-        Path to where data on projects should be stored. Default: None
 
     Returns
     -------
@@ -103,17 +95,15 @@ def plot_commits_by_time(project=None, since='2018-08-07', frequency='10H',
     dates = pd.date_range(since, datetime.datetime.now(), freq=frequency)
 
     # get project info
-    info = get_project_info(PROJECT_LIST, datadir=datadir,
-                            since=since, verbose=False)[0]
     if project is not None:
         if not isinstance(project, list):
             project = [project]
-        info = info[info.project.isin(project)]
+        commits = commits[commits.project.isin(project)]
 
     # compile number of commits over time for each project
     plot = pd.DataFrame([], columns=['time', 'commits',  'project'])
-    for proj in info.project.unique():
-        cp = info.query(f'project=="{proj}"')
+    for proj in commits.project.unique():
+        cp = commits.query(f'project=="{proj}"')
         num_commits = np.array([np.sum(cp.date < d) for d in dates])
         plot = plot.append(pd.DataFrame(dict(time=dates,
                                              commits=num_commits,
@@ -121,51 +111,50 @@ def plot_commits_by_time(project=None, since='2018-08-07', frequency='10H',
     plot.commits = plot.commits.astype(int)
 
     # make plot!
-    ax = sns.lineplot(x='time', y='commits', hue='project', data=plot)
+    ax = sns.lineplot(x='time', y='commits', hue='project', data=plot, ax=ax)
     sns.despine(ax=ax)
     ax.set(xlabel='Time', ylabel='Number of commits',
-           ylim=[0, ax.get_ylim()[-1] + 10])
+           ylim=[0, ax.get_ylim()[-1] + 10],
+           title='Cumulative commits across time')
     ax.set_xticklabels(ax.get_xticklabels(), rotation=45)
     ax.figure.tight_layout()
 
     return ax
 
 
-def scatter_by_statistics(project=None, hue='project', since='2018-08-07',
-                          datadir=None):
+def scatter_by_statistics(commits, project=None, hue='project', ax=None):
     """
     Makes scatterplot of commits in `project` by additions + deletions
 
     Parameters
     ----------
+    commits : pd.core.data.DataFrame
+        Information on commits from projects
     project : str, optional
         Name of project to plot commits from
     hue : {'project', 'user'}, optional
         One of 'project' or 'user'
-    since : str, optional
-        Date (YYYY-MM-DD) from which to pull information on projects:
-        Default: 2018-08-07
-    datadir : str, optional
-        Path to where data on projects should be stored. Default: None
 
     Returns
     -------
     ax : matplotlib.axes.Axis
         Axis with plot
     """
+    data = commits.copy()
+
     # get project info
-    info = get_project_info(PROJECT_LIST, datadir=datadir,
-                            since=since, verbose=False)[0]
     if project is not None:
         if not isinstance(project, list):
             project = [project]
-        info = info[info.project.isin(project)]
+        data = data[data.project.isin(project)]
         hue = 'user'
-    info.additions = info.additions.copy().apply(np.log)
-    info.deletions = info.deletions.copy().apply(np.log)
+    data.loc[:, 'additions'] = data.additions.copy().apply(np.log)
+    data.loc[:, 'deletions'] = data.deletions.copy().apply(np.log)
 
     # make plot!
-    ax = sns.scatterplot(x='additions', y='deletions', hue=hue, data=info)
+    ax = sns.scatterplot(x='additions', y='deletions', hue=hue, data=data,
+                         ax=ax)
+    ax.legend_.set_bbox_to_anchor((1.0, 1.0))
     ax.set(xlabel='Lines of code added (log)',
            ylabel='Lines of code deleted (log)')
     sns.despine(ax=ax)
@@ -173,56 +162,139 @@ def scatter_by_statistics(project=None, hue='project', since='2018-08-07',
     return ax
 
 
-def plot_issues_by_project(status='opened', by_time=True, since='2018-08-07',
-                           frequency='10H', datadir=None):
+def _prepare_issues(issues, since='2018-08-07', frequency='10H'):
     """
-    Makes plot of issues created in `project` over time
+    Prepares `issues` into slightly tidier data frame for plotting
 
     Parameters
     ----------
-    status : {'opened', 'closed'}, optional
-        Whether to plot issues that were 'opened' or 'closed'
+    issues : pd.core.data.DataFrame
+        Information on issues from projects
     since : str, optional
         Date (YYYY-MM-DD) from which to pull information on projects:
         Default: 2018-08-07
     frequency : str, optional
-        Time chunks with which to plot commits over time. Default: '10H'
-    datadir : str, optional
-        Path to where data on projects should be stored. Default: None
+        Time chunks with which to group issues. Default: '10H'
+
+    Returns
+    -------
+    issues : pd.core.data.DataFrame
+        Organized issues!
+    """
+    dates = pd.date_range(since, datetime.datetime.now(), freq=frequency)
+
+    # get project info
+    data = pd.DataFrame([], columns=['time', 'number',
+                                     'status', 'project'])
+    for proj in issues.project.unique():
+        cp = issues.query(f'project=="{proj}"')
+        ci, oi = [], []
+        for n, d in enumerate(dates[1:], 1):
+            low, high = dates[n-1], d
+            col = 'closed_at'
+            ci += [len(cp.dropna(subset=[col])
+                         .query(f'{col} < "{high}" & {col} >= "{low}"'))]
+            col = 'created_at'
+            oi += [len(cp.dropna(subset=[col])
+                         .query(f'{col} < "{high}" & {col} >= "{low}"'))]
+        inputs = dict(time=np.tile(dates[1:], 2),
+                      number=np.hstack([oi, ci]),
+                      status=np.hstack([np.repeat('opened', len(oi)),
+                                        np.repeat('closed', len(ci))]),
+                      project=proj)
+        data = data.append(pd.DataFrame(inputs), sort=True)
+    data.number = data.number.astype(int)
+
+    return data
+
+
+def plot_issues_by_project(issues, project=None, since='2018-08-07',
+                           frequency='10H', ax=None):
+    """
+    Makes plot of `issues` by time (by project if not `by_time`)
+
+    Parameters
+    ----------
+    issues : pd.core.data.DataFrame
+        Information on issues from projects
+    project : str, optional
+        If `by_time`, the name of the project from which to plot issues
+    by_time : bool, optional
+        Whther to plot issues as a function of time instead of project.
+        Default: True
+    since : str, optional
+        Date (YYYY-MM-DD) from which to pull information on projects:
+        Default: 2018-08-07
+    frequency : str, optional
+        Time chunks with which to plot issues. Default: '10H'
 
     Returns
     -------
     ax : matplotlib.axes.Axis
         Axis with plot
     """
-    dates = pd.date_range(since, datetime.datetime.now(), freq=frequency)
+    data = _prepare_issues(issues, since=since, frequency=frequency)
+    data = (data.drop('time', axis=1)
+                .groupby(['project', 'status'])
+                .sum()
+                .reset_index())
+    closed = data.query('status=="closed"')
+    opened = data.query('status=="opened"')
+    if ax is None:
+        fig, ax = plt.subplots(1, 1)
+    ax.barh(closed.project, closed.number, label='Closed issues')
+    plt.barh(opened.project, opened.number, left=closed.number,
+             label='Open issues')
+    ax.set(xlabel='Number of issues')
+    ax.figure.tight_layout()
+    plt.legend()
+
+    sns.despine(ax=ax)
+
+    return ax
+
+
+def plot_issues_by_time(issues, project=None, since='2018-08-07',
+                        frequency='10H', ax=None):
+    """
+    Makes plot of `issues` by time (by project if not `by_time`)
+
+    Parameters
+    ----------
+    issues : pd.core.data.DataFrame
+        Information on issues from projects
+    project : str, optional
+        If `by_time`, the name of the project from which to plot issues
+    since : str, optional
+        Date (YYYY-MM-DD) from which to pull information on projects:
+        Default: 2018-08-07
+    frequency : str, optional
+        Time chunks with which to plot issues. Default: '10H'
+
+    Returns
+    -------
+    ax : matplotlib.axes.Axis
+        Axis with plot
+    """
+    data = _prepare_issues(issues, since=since, frequency=frequency)
 
     # get project info
-    info = get_project_info(PROJECT_LIST, datadir=datadir,
-                            since=since, verbose=False)[1]
-
-    data = pd.DataFrame([], columns=['time', 'opened',
-                                     'closed', 'project'])
-    for proj in info.project.unique():
-        cp = info.query(f'project=="{proj}"')
-        oi = np.array([np.sum(cp.created_at.dropna() < d) for d in dates])
-        ci = np.array([np.sum(cp.closed_at.dropna() < d) for d in dates])
-        data = data.append(pd.DataFrame(dict(time=dates,
-                                             opened=oi,
-                                             closed=ci,
-                                             project=proj)),
-                           sort=True)
-    data.opened = data.opened.astype(int)
-    data.closed = data.closed.astype(int)
-
-    # make plot!
-    if by_time:
-        ax = sns.lineplot(x='time', y=status, hue='project', data=data)
-    else:
-        data = data.groupby('project').max()[status]
-        ax = sns.barplot(data.values, data.index)
-        ax.figure.tight_layout()
-    ax.set(ylabel='Issues {}'.format(status))
+    if project is not None:
+        if not isinstance(project, list):
+            project = [project]
+        data = data[data.project.isin(project)]
+    hue = None if len(project) == 1 else 'project'
+    ax = sns.lineplot(x='time', y='number', hue=hue, style='status',
+                      data=data, ax=ax)
+    xticklabels = (data.time
+                       .apply(lambda x: x.strftime('%Y-%m-%d %H'))
+                       .unique())
+    ax.set(xticks=xticklabels,
+           title=project[0] if hue is None else '')
+    ax.legend_.set_bbox_to_anchor((1.0, 1.0))
+    ax.set_xlabel('Time', labelpad=15)
+    ax.set_ylabel('Number of issues', labelpad=15)
+    ax.set_xticklabels(xticklabels, rotation=45)
     sns.despine(ax=ax)
 
     return ax
